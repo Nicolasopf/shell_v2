374d57fa342829de12c4a30859851da6d634c680
Nicolas <n.ico15935746@gmail.com>
2021-08-16 15:11:34 -0500

    [Added] isnum feature

---
+++ strings/string_utils3.c
@@ -0,0 +1,21 @@
+#include "../hsh.h"
+
+/**
+ * _isnum - check if a string is num
+ * @str: input str
+ * Return: 1 is str is a number or 0 if is not
+ */
+
+int _isnum(const char * const str)
+{
+	int i;
+	const char *runner = str;
+
+	if (*runner == '-')
+		runner++;
+
+	for (i = 0; runner[i]; i++)
+		if (runner[i] < '0' || '9' < runner[i])
+			return (0);
+	return (1);
+}
