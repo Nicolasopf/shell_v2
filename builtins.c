97e227dcc51883ad8b1338bad9cabe68b06994e4
Nicolas <n.ico15935746@gmail.com>
2021-08-16 15:04:04 -0500

    [Added] Builtins exit, printenv, etc

---
+++ builtins.c
@@ -0,0 +1,111 @@
+#include "hsh.h"
+
+int _printenv(void);
+int _bexit(char **argv);
+int _chdir(char **argv);
+
+/**
+ * _builtins - check for builtins functions
+ * @argv: input arguments
+ * Return: 1=BREAK 2=CONTINUE 3=NOTFOUND
+ */
+
+int _builtins(char **argv)
+{
+	int status = NOTFOUND;
+	char *cmd = argv[0];
+
+	if (_strncmp(cmd, "exit", _strlen("exit")) == 0)
+		status = _bexit(argv);
+	else if (_strncmp(cmd, "cd", _strlen("cd")) == 0)
+		status = _chdir(argv);
+	else if (_strncmp(cmd, "env", _strlen("env")) == 0)
+		status = _printenv();
+
+	return (status);
+}
+
+/**
+ * _chdir - change directory
+ * @argv: arguments for the program
+ * Return: BREAK=1 CONTINUE=2
+ */
+
+int _chdir(char **argv)
+{
+	int status = CONTINUE, print_old = 0;
+	int *program_status = &globals()->program_status;
+	char *working_dir = _getenv("PWD"), *home = _getenv("HOME");
+	char *old = _getenv("OLDPWD"), *dst = argv[1], *cwd;
+
+	*program_status = 0;
+
+	if (!dst)
+		dst = home;
+
+	else if (_strcmp(dst, "-") == 0)
+		print_old = 1, dst = old;
+
+	if (dst)
+	{
+		status = chdir(dst);
+		if (status != -1)
+			_strcpy(working_dir, dst);
+		else
+		{
+			_perror(3, dst);
+			*program_status = 2;
+		}
+	}
+
+	if (print_old)
+	{
+		cwd = getcwd(NULL, 0);
+		puts(cwd);
+		free(cwd);
+	}
+
+	return (status);
+}
+
+/**
+ * _printenv - print environment
+ * Return: 1=CONTINUE
+ */
+
+int _printenv(void)
+{
+	int i;
+
+	if (environ)
+		for (i = 0; environ[i]; i++)
+			printf("%s\n", environ[i]);
+	return (CONTINUE);
+}
+
+/**
+ * _bexit - builtin exit
+ * @argv: arguments for the program
+ * Return: BREAK=1 CONTINUE=2
+ */
+int _bexit(char **argv)
+{
+	int status, tmp;
+	int *program_status = &globals()->program_status;
+
+	status = BREAK;
+	if (argv[1])
+	{
+		if (_isnum(argv[1]))
+		{
+			tmp = _atoi(argv[1]);
+			*program_status = tmp;
+			if (tmp >= 0)
+				return (BREAK);
+		}
+		_perror(2, argv[1]);
+		*program_status = 2;
+		status = CONTINUE;
+	}
+	return (status);
+}
